95f964f872c955b9d04c6602229d9e7cc46835e4
Akash Jaiswal <akashjas1996@gmail.com>
2019-11-07 16:47:25 +0530

    webhook changed

--- payment/success.php
+++ payment/success.php
@@ -2,13 +2,13 @@
 include '../inc/dbconnection.php';
 $ch = curl_init();
 $vars=$_GET['payment_request_id'];
-curl_setopt($ch, CURLOPT_URL, 'https://test.instamojo.com/api/1.1/'.$vars.'/');
+curl_setopt($ch, CURLOPT_URL, 'https://www.instamojo.com/api/1.1/payment-requests/'.$vars.'/');
 curl_setopt($ch, CURLOPT_HEADER, FALSE);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
 curl_setopt($ch, CURLOPT_HTTPHEADER,
-            array("X-Api-Key:test_d73f7f56f5629d084e4e8aeb22b",
-                  "X-Auth-Token:test_d73f7f56f5629d084e4e8aeb22b"));
+            array("X-Api-Key:95f8aaec011f218da370a3cc478b1954",
+                  "X-Auth-Token:a6dc9e938dbd556dfd35cee2cfd7c9d5"));
 $response = curl_exec($ch);
 
 $phpvar = json_decode($response);
